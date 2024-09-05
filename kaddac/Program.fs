c3526cb82a17d479c2df0c239bf9cb84b9c8bce3
Meerie33 <timwulsch@outlook.de>
2024-09-05 12:23:54 +0200

    Simple F# implementation
    currently not working

---
+++ kaddac/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+printfn "Hello from F#"
+let input = Console.ReadLine()
+
+if input = "ok" then 
+    printfn "no errors"
+else
+    printfn $"ERROR: Unexpected {input}, Syntax error"
